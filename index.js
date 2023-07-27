8e22262a852e7c013321ef46bba4b21971b4b784
Mohit soni <sonimohit73180@gmail.com>
2023-07-27 10:55:02 +0530

    JWT install

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ app.get('/getFeedback',urlencodedParser,(req,res)=>{
 //     })
 // })
 
+
 app.post('/feedback',urlencodedParser,  (req, res)=> {
     var data = req.body;
     var feedData = [data.Name,data.EmailId,data.Gender,data.Phone,data.Rating,data.Thoughts,data.Parameters]
@@ -46,4 +47,57 @@ app.post('/feedback',urlencodedParser,  (req, res)=> {
    
  });
 
+ app.get('/businessDetails',urlencodedParser,(req,res)=>{
+    connection.query('SELECT * FROM business_table',(err,rows)=>{
+        if(err){
+            console.log(err);
+        }
+        else{
+          res.send(rows);
+        }
+    })
+})
+
+ app.post('/businessCreate',urlencodedParser,  (req, res)=> {
+    var data = req.body;
+    var B_Data = [data.B_name,data.Address,data.Phone,data.Email,data.Personal_name,data.Remark,data.B_logo]
+    connection.query('INSERT INTO business_table(B_name,Address,Phone,Email,Personal_name,Remark,B_logo) values(?)',[B_Data],(err,rows)=>{
+        if(err){
+            console.log(err);
+        }
+        else{
+          res.send(rows);
+        }
+    })
+   
+ });
+
+ app.put('/updateBusinessDetails',urlencodedParser,  (req, res)=> {
+    var data = req.body;
+    var B_Data = [data.B_name,data.Address,data.Phone,data.Email,data.Personal_name,data.Remark,data.B_logo,data.Business_id]
+    connection.query('UPDATE business_table SET B_name=?,Address=?,Phone=?,Email=?,Personal_name=?,Remark=?,B_logo=? where Business_id = ?',B_Data,(err,rows)=>{
+        if(err){
+            console.log(err);
+        }
+        else{
+          res.send(rows);
+        }
+    })
+   
+ });
+
+ app.delete('/deleteBusiness/:id',urlencodedParser,(req,res)=>{
+    const id = req.params.id;
+   console.log(id);
+    connection.query("DELETE FROM business_table WHERE Business_id ="+id,(err,result)=>{
+        if(err){
+        console.log(err);
+        res.send(err)
+    }
+    else{
+      res.send(result);
+    }
+    });
+
+ })
 app.listen(3000,()=>console.log("Express server running port 3000"));
